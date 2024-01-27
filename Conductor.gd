4ff1a9f7c5e082b7fdbc19dff61d6bcd5382e16f
jt <jt@modernbanc.com>
2024-01-26 20:04:38 -0600

    finished conductor node

--- Conductor.gd
+++ Conductor.gd
@@ -50,6 +50,26 @@ func closest_beat(nth):
 	time_off_beat = abs(closest * sec_per_beat - song_position)
 	return Vector2(closest, time_off_beat)
 
+func play_from_beat(beat, offset):
+	play()
+	seek(beat * sec_per_beat)
+	beats_before_start = offset
+	
+	measure = beat % measures
+	
+func _on_StartTimer_timeout():
+	song_position_in_beats += 1
+	if song_position_in_beats < beats_before_start - 1:
+		$StartTimer.start()
+	elif song_position_in_beats == beats_before_start - 1:
+		$StartTimer.wait_time = $StartTimer.wait_time - (AudioServer.get_time_to_next_mix() + 
+														AudioServer.get_output_latency())
+		$StartTimer.start()
+	else:
+		play()
+		$StartTimer.stop()
+	_report_beat()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
